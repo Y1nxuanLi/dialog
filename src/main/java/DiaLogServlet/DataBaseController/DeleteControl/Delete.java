2c3da1796379d785ef6c6d24abbcbc79abd1af39
Y1nxuanLi <yinxuan.li21@imperial.ac.uk>
2024-01-07 21:45:40 +0000

    Testing Home Page v4.1.6, Servlet classes are expected to be concrete classes instead of abstract

--- src/main/java/DiaLogServlet/DataBaseController/DeleteControl/Delete.java
+++ src/main/java/DiaLogServlet/DataBaseController/DeleteControl/Delete.java
@@ -7,18 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 
-@WebServlet(urlPatterns={"/api/post/delete"}, loadOnStartup=1)
 public abstract class Delete extends HttpServlet implements Deleteable{
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        String servletPath = req.getServletPath();
-
-        switch (servletPath) {
-            case "/api/post/delete":
-                delete(req, resp);
-        }
-    }
-
     public abstract void delete(HttpServletRequest req, HttpServletResponse resp) throws IOException;
 }
 
--- src/main/java/DiaLogServlet/DataBaseController/ReadControl/Read.java
+++ src/main/java/DiaLogServlet/DataBaseController/ReadControl/Read.java
@@ -8,17 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns={"/api/post/read"}, loadOnStartup=1)
 public abstract class Read extends HttpServlet implements Readable {
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        String servletPath = req.getServletPath();
-        switch (servletPath) {
-            case "/api/post/read":
-                read(req, resp);
-        }
-
-    }
-
     public abstract void read(HttpServletRequest req, HttpServletResponse resp) throws IOException;
 }

f7d1f273bca344afd68525a538fc22b26a88101c
Y1nxuanLi <yinxuan.li21@imperial.ac.uk>
2024-01-14 02:18:25 +0000

    logbook v5.13.8

--- src/main/java/DiaLogServlet/Admin/TestServlet3.java
+++ src/main/java/DiaLogServlet/Admin/TestServlet3.java
@@ -24,32 +24,33 @@ public class TestServlet3 extends HttpServlet {
             case "/LogDataTesting":
                 resp.getWriter().write("LogData Display for testing purpose: \n");
 
-                // Test data for insertData method
-                int userID = 1;
-                String bloodSugar = "12";
-                String notes = "";
-                String createTime = "2024-01-03 08:00:00";
-                String updateTime = "";
-                String logType = "2";
-                String carb = "45";
-                String mealDescription = "Breakfast";
-                String insulinDose = "";
-                String medication = "";
-                String exerciseDescription = "";
-                String exerciseType = "";
-                String exerciseDuration = "";
-                String insulinType = "";
+//                // Test data for insertData method
+//                int userID = 1;
+//                String bloodSugar = "12";
+//                String notes = "";
+//                String createTime = "2024-01-03 08:00:00";
+//                String updateTime = "";
+//                String logType = "2";
+//                String carb = "45";
+//                String mealDescription = "Breakfast";
+//                String insulinDose = "";
+//                String medication = "";
+//                String exerciseDescription = "";
+//                String exerciseType = "";
+//                String exerciseDuration = "";
+//                String insulinType = "";
+//
+//                LogDataSQL.insertData(userID, bloodSugar, notes, createTime, updateTime, logType, carb, mealDescription, insulinDose, medication, exerciseDescription, exerciseType, exerciseDuration, insulinType);
+//
+//
+//                int logID = 1;
+//                String updatedBloodSugar = "5";
+//                String updatedNotes = "Feeling better";
+//                String updatedUpdateTime = "2024-01-13 19:00:00";
+//
+//
+//                LogDataSQL.updateLog(logID, userID, updatedBloodSugar, updatedNotes, createTime, updatedUpdateTime, logType, carb, mealDescription, insulinDose, medication, exerciseDescription, exerciseType, exerciseDuration, insulinType);
 
-                LogDataSQL.insertData(userID, bloodSugar, notes, createTime, updateTime, logType, carb, mealDescription, insulinDose, medication, exerciseDescription, exerciseType, exerciseDuration, insulinType);
-
-
-                int logID = 1;
-                String updatedBloodSugar = "5";
-                String updatedNotes = "Feeling better";
-                String updatedUpdateTime = "2024-01-13 19:00:00";
-
-
-                LogDataSQL.updateLog(logID, userID, updatedBloodSugar, updatedNotes, createTime, updatedUpdateTime, logType, carb, mealDescription, insulinDose, medication, exerciseDescription, exerciseType, exerciseDuration, insulinType);
                 LogDataSQL.displayData(resp);
                 JsonArray logsArray = LogDataSQL.readAllLogs(1);
                 resp.getWriter().write(String.valueOf(logsArray));
@@ -75,3 +76,6 @@ public class TestServlet3 extends HttpServlet {
 //https://dialog-1d1125195912.herokuapp.com/login
 //https://dialog-1d1125195912.herokuapp.com/register
 
+//{"userId":1,"bloodSugar":"45","notes":"","createTime":"","updateTime":"","logType":"","carb":"","mealDescription":"","insulinDose":"","medication":"","exerciseDescription":"","exerciseType":"","exerciseDuration":"","insulinType":""}
+//
+//{"code":200,"message":"SUCCESS","data":[{"id":2,"userId":1,"bloodSugar":"12","notes":"","createTime":"2024-01-03 08:00:00","updateTime":"","logType":2,"carb":"45","mealDescription":"Breakfast","insulinDose":"","medication":"","exerciseDescription":"","exerciseType":"","exerciseDuration":"","insulinType":""},{"id":1,"userId":1,"bloodSugar":"5","notes":"Feeling better","createTime":"2024-01-03 08:00:00","updateTime":"2024-01-13 19:00:00","logType":2,"carb":"45","mealDescription":"Breakfast","insulinDose":"","medication":"","exerciseDescription":"","exerciseType":"","exerciseDuration":"","insulinType":""}]}
